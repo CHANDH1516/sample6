edaf1ac87cabbd76b2283a7880485eaa557bd1a5
CHANDH1516 <22h51a0546@cmrcet.ac.com>
2024-12-08 10:58:28 +0530

    prime numbers

---
+++ primenumbers.java
@@ -0,0 +1,17 @@
+public class primenumbers {
+    public static void main(String[] args) {
+        int i, num, count;
+        System.out.println("Prime numbers from 1 to 100 are:");
+        for (i = 1; i <= 100; i++) {
+            count = 0;
+            for (num = i; num >= 1; num--) {
+                if (i % num == 0) {
+                    count++;
+                }
+            }
+            if (count == 2) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
